5c3df2e38f4b5edb10f7042b6db766442a89f4ab
EcenurOrhan <72939985+EcenurOrhan@users.noreply.github.com>
2020-10-22 05:46:30 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello World!!!!");
+
+    }
+}
